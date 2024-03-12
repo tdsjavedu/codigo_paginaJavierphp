8080bfb43434536b0c20fe66635ee437e54a4489
Javier Osorio <24004208@galileo.edu>
2024-03-12 04:55:48 +0000

    Primer Commit

---
+++ paginajavier.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <div style="font-size: 20px;">
+    <?php
+    echo "Hola mundo, mi nombre es Javier Osorio!";
+    ?>
+</head>
+<body>
+
+<img src="https://cdn.leonardo.ai/users/07e1d84d-07a2-4b2e-95d6-63cf5501aca5/generations/611fbd53-ed38-4ff0-a8f7-8781067cd212/Default_Quetzal_hacker_mask_2.jpg" alt="Example Image" width="200" height="150">
+
+</body>
+</html>
+
+
